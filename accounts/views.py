66343af54541cb69626439977d9d5a46a4578978
asifalam515 <asibulalam515@gmail.com>
2024-01-17 14:08:24 +0600

    login feature applied

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import FormView
 from  .forms import UserRegistrationForm
-from django.contrib.auth import login
+from django.contrib.auth import login,logout
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView,LogoutView
+
 # Create your views here.
 class UserRegistrationView(FormView):
     template_name = 'accounts/user_registration.html'
@@ -17,4 +19,13 @@ class UserRegistrationView(FormView):
         return super().form_valid(form)  #form_valid function call hobe jodi sob thik thake
     
     
-    
+class UserLoginView(LoginView):
+    template_name ='accounts/user_login.html'
+    def get_success_url(self):
+        return reverse_lazy('home') 
+
+class UserLogoutView(LogoutView):
+    def get_success_url(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return reverse_lazy('home')
